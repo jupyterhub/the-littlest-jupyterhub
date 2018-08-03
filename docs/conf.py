82e529cf1250dd78a941143a3d5ffb94dc360de5
Tim Head <betatim@gmail.com>
2018-08-03 16:29:00 +0200

    Explicitly exclude file that is not in any TOC

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,8 @@ master_doc = 'index'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
+                    'tutorials/custom.rst']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
