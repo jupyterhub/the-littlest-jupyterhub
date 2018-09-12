9b0248e0c3afe95855ec126855ed8450cc7e05fb
yuvipanda <yuvipanda@gmail.com>
2018-09-12 16:46:59 -0700

    Normalize unix usernames to be under 32char
    JupyterHub usernames do not have this restriction,
    causing user creation to fail when usernames are too large.

---
+++ tljh/normalize.py
@@ -0,0 +1,24 @@
+"""
+Functions to normalize various inputs
+"""
+import hashlib
+
+
+def generate_system_username(username):
+    """
+    Generate a posix username from given username.
+
+    If username < 26 char, we just return it.
+    Else, we hash the username, truncate username at
+    26 char, append a '-' and first add 5char of hash. 
+    This makes sure our usernames are always under 32char.
+    """
+
+    if len(username) < 26:
+        return username
+
+    userhash = hashlib.sha256(username.encode('utf-8')).hexdigest()
+    return '{username_trunc}-{hash}'.format(
+        username_trunc=username[:26],
+        hash=userhash[:5]
+    )
