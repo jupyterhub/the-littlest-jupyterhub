ca38dcd41337780a6d1408ba589c3a2e9b7bb41b
yuvipanda <yuvipanda@gmail.com>
2018-11-13 14:40:24 -0800

    Decode output from subprocess before performing string operations

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -29,7 +29,7 @@ def get_os_release_variable(key):
     return subprocess.check_output([
         '/bin/bash', '-c',
         "source /etc/os-release && echo ${{{key}}}".format(key=key)
-    ]).trim()
+    ]).decode().trim()
 
 def main():
 
