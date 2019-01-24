29d4419e49026330671ebeda073f899b6486e461
Willi Rath <willirath@users.noreply.github.com>
2019-01-24 18:31:26 +0100

    Use newer firstuseauthenticator
    This uses v0.12 which allow for deleting users before they have set their password.

--- tljh/installer.py
+++ tljh/installer.py
@@ -187,7 +187,7 @@ def ensure_jupyterhub_package(prefix):
         'jupyterhub==0.9.4',
         'jupyterhub-dummyauthenticator==0.3.1',
         'jupyterhub-systemdspawner==0.11',
-        'jupyterhub-firstuseauthenticator==0.11',
+        'jupyterhub-firstuseauthenticator==0.12',
         'jupyterhub-ldapauthenticator==1.2.2',
         'oauthenticator==0.8.0',
     ])
