e47d6a4473dd288456bab0032b5fa0d83f229278
GeorgianaElena <georgiana.dolocan@gmail.com>
2020-01-08 10:54:17 +0200

    Upgrade authenticators

--- tljh/installer.py
+++ tljh/installer.py
@@ -205,11 +205,11 @@ def ensure_jupyterhub_package(prefix):
         'jupyterhub==1.0.0',
         'jupyterhub-dummyauthenticator==0.3.1',
         'jupyterhub-systemdspawner==0.13',
-        'jupyterhub-firstuseauthenticator==0.12',
+        'jupyterhub-firstuseauthenticator==0.13.0',
         'jupyterhub-nativeauthenticator==0.0.4',
         'jupyterhub-ldapauthenticator==1.2.2',
         'jupyterhub-tmpauthenticator==0.6',
-        'oauthenticator==0.8.2',
+        'oauthenticator==0.10.0',
     ])
     traefik.ensure_traefik_binary(prefix)
 
