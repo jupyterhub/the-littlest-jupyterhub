f82c49c12c14e68b236c30536900ccf9177dc999
YuviPanda <yuvipanda@gmail.com>
2020-05-08 16:48:52 +0530

    Use sha256 sums for verifying miniconda download
    miniconda downloader now provides sha256 hashes. Previously
    it provided only md5 hashes. md5 is deprecated everywhere for
    everything, so let's use this

--- tests/test_conda.py
+++ tests/test_conda.py
@@ -14,9 +14,10 @@ def prefix():
     Provide a temporary directory with a conda environment
     """
     miniconda_version = '4.7.10'
-    miniconda_installer_md5 = "1c945f2b3335c7b2b15130b1b2dc5cf4"
+    miniconda_installer_sha256 = "8a324adcc9eaf1c09e22a992bb6234d91a94146840ee6b11c114ecadafc68121"
+    installer_url = "https://repo.continuum.io/miniconda/Miniconda3-{}-Linux-x86_64.sh".format(miniconda_version)
     with tempfile.TemporaryDirectory() as tmpdir:
-        with conda.download_miniconda_installer(miniconda_version, miniconda_installer_md5) as installer_path:
+        with conda.download_miniconda_installer(installer_url, miniconda_installer_sha256) as installer_path:
             conda.install_miniconda(installer_path, tmpdir)
         conda.ensure_conda_packages(tmpdir, [
             'conda==4.8.1'
