a4768226eeb2ac56996432ae176967dcd9a7f52e
Erik Sundell <erik.i.sundell@gmail.com>
2021-10-18 02:05:44 +0200

    ci: make pytest report time per test

--- .github/integration-test.py
+++ .github/integration-test.py
@@ -116,7 +116,7 @@ def run_test(image_name, test_name, bootstrap_pip_spec, test_files, upgrade, ins
     )
     run_container_command(
         test_name,
-        '/opt/tljh/hub/bin/python3 -m pytest --verbose --maxfail=2 --color=yes --capture=no {}'.format(
+        '/opt/tljh/hub/bin/python3 -m pytest --verbose --maxfail=2 --color=yes --durations=10 --capture=no {}'.format(
             ' '.join([os.path.join('/srv/src/integration-tests/', f) for f in test_files])
         )
     )
