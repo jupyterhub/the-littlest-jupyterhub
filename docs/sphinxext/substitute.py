84f8c1fb48d5623a39c476d7047093869f486bc4
iameskild <eskild@doublee.io>
2022-03-14 19:32:24 +0100

    Add docstring

--- docs/sphinxext/substitute.py
+++ docs/sphinxext/substitute.py
@@ -3,6 +3,11 @@ Substitute placeholder values in the docs with values set in conf.py.
 """
 
 def ubuntu_version_sub(app, docname, source):
+    """
+    Replace `||UBUNTU_PREVIOUS_VERSION||` and `||UBUNTU_LATEST_VERSION||` with the supported versions of Ubuntu.
+
+    As new versions of Ubuntu are released, update the `ext_ubuntu_latest_version` and `ext_ubuntu_previous_version` in the `conf.py`.
+    """
     if app.config.ext_ubuntu_latest_version != "" and app.config.ext_ubuntu_previous_version != "":
         src = source[0]
         source[0] = (src
