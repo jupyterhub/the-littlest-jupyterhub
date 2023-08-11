fc8e19b1b5663f58f0e7b089903d1d1769db06b8
Erik Sundell <erik.i.sundell@gmail.com>
2023-08-11 14:46:14 +0200

    Bump to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="the-littlest-jupyterhub",
-    version="1.0.0b1",
+    version="1.0.0",
     description="A small JupyterHub distribution",
     url="https://github.com/jupyterhub/the-littlest-jupyterhub",
     author="Jupyter Development Team",
