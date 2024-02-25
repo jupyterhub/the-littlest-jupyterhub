2fe9912333c6d125fd0291bc7b0ffab30e8822ae
Erik Sundell <erik@sundellopensource.se>
2024-02-25 15:04:14 +0100

    tests: test pip install plugin with no-dependency package
    This is a simplification to help ensure we don't run into issues like
    below because requests gets installed via pip.
    
    ```
    RemoveError: 'requests' is a dependency of conda and cannot be removed from conda's operating environment.
    ```

--- integration-tests/test_simplest_plugin.py
+++ integration-tests/test_simplest_plugin.py
@@ -20,7 +20,7 @@ def test_tljh_extra_user_conda_packages():
 
 
 def test_tljh_extra_user_pip_packages():
-    subprocess.check_call([f"{USER_ENV_PREFIX}/bin/python3", "-c", "import django"])
+    subprocess.check_call([f"{USER_ENV_PREFIX}/bin/python3", "-c", "import simplejson"])
 
 
 def test_tljh_extra_hub_pip_packages():
